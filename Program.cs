e7b6e3dc224e3165f3b8515c4858f88097c53e2a
Stefan <Stefan@DESKTOP-P1OADI2>
2024-11-05 14:53:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Assignment 2.1";
+
+            Console.WriteLine("This program checks if two number are equal, please enter two numebrs and press enter after each one");
+
+            double numOne = Convert.ToDouble(Console.ReadLine());
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+
+            if (numOne == numTwo)
+            {
+                Console.WriteLine("These numbers are equal!");
+
+            }
+            else
+            {
+                Console.WriteLine("These numebrs are not equal");
+            }
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
